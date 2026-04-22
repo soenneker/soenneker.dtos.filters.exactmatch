09d0de14c375c0544422c97360c741ad3b3f27f0
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:39 -0500

    TUnit conversion

--- test/Soenneker.Dtos.Filters.ExactMatch.Tests/ExactMatchFilterTests.cs
+++ test/Soenneker.Dtos.Filters.ExactMatch.Tests/ExactMatchFilterTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Dtos.Filters.ExactMatch.Tests;
 
 public sealed class ExactMatchFilterTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
